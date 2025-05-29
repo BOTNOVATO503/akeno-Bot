10806de5df3990cb4b24156e562edfb11c908850
BOTNOVATO503 <lopeztatu25@gmail.com>
2025-05-28 19:55:18 -0600

    Update config.jss

--- config.js
+++ config.js
@@ -87,13 +87,13 @@ ch3: '120363341523880410@newsletter',
 }
 
 //• ↳ ◜𝑳𝑰𝑵𝑲𝑺◞ • 🌿
-global.gp4 = 'https://whatsapp.com/channel/0029Vaqe1Iv65yDAKBYr6z0A' //Channel Team infinite
-global.gp1 = 'https://whatsapp.com/channel/0029Vaqe1Iv65yDAKBYr6z0A' //Grupo 
-global.gp2 = 'https://whatsapp.com/channel/0029Vaqe1Iv65yDAKBYr6z0A'//
-global.comunidad1 = 'https://whatsapp.com/channel/0029Vaqe1Iv65yDAKBYr6z0A' //Comunidad 𝘼𝙠𝙚𝙣𝙤 𝙝𝙞𝙢𝙚𝙟𝙞𝙢𝙖-𝘽𝙊𝙏
-global.channel = 'https://whatsapp.com/channel/0029Vaqe1Iv65yDAKBYr6z0A' //Canal Oficial
-global.channel2 = 'https://whatsapp.com/channel/0029VbB8ooKK5cD7K3akFP35' //Canal test
-global.yt = 'https://youtube.com/@kakaroto-bot?si=dRYFi-dAw8H_mgGI' //Canal De Youtube
+global.gp4 = 'https://chat.whatsapp.com/I7WiWv6cuLJE6x3DiThgzx' //Channel Team infinite
+global.gp1 = 'https://whatsapp.com/channel/0029Vaqe1Iv65yDAKBYr6zA' //Grupo 
+global.gp2 = 'https://whatsapp.com/channel/0029Vaqe1Iv65yDAKBYr6zA'//
+global.comunidad1 = 'https://whatsapp.com/channel/0029Vaqe1Iv65yDAKBYr6zA' //Comunidad 𝘼𝙠𝙚𝙣𝙤 𝙝𝙞𝙢𝙚𝙟𝙞𝙢𝙖-𝘽𝙊𝙏
+global.channel = 'https://chat.whatsapp.com/I7WiWv6cuLJE6x3DiThgzx' //Canal Oficial
+global.channel2 = 'https://chat.whatsapp.com/I7WiWv6cuLJE6x3DiThgzx' //Canal test
+global.yt = 'https://youtube.com/@kakaroto-bot?si=dRYFi-dAw8H_mgG' //Canal De Youtube
 global.md = 'https://github.com/TOKIO5025' //Github Oficial
 global.correo = 'albertodovi100@gmail.com'
 global.cn ='https://whatsapp.com/channel/0029Vaqe1Iv65yDAKBYr6z0A';
